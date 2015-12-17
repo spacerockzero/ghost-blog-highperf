68d3c7994aba39b4a790485e09bbabb52aa7eb33
spacerockzero <janderson@familysearch.org>
2015-12-16 22:18:04 -0700

    removed jQuery and prefetch

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,11 @@ var watch     = require('gulp-watch')
 
 /* CONFIGS */
 var jsSrc = [
-  'bower_components/jquery/dist/jquery.min.js',
-  'content/themes/casper/assets/js/jquery.fitvids.js',
+  // 'bower_components/jquery/dist/jquery.min.js',
+  // 'content/themes/casper/assets/js/jquery.fitvids.js',
   'content/themes/casper/assets/js/index.js',
-  'node_modules/prefetch/prefetch.js',
-  'content/themes/casper/assets/js/app.js'
+  // 'node_modules/prefetch/prefetch.js',
+  // 'content/themes/casper/assets/js/app.js'
 ]
 
 /* MINIFY CSS CONFIG */
@@ -29,14 +29,14 @@ var uglifyConfig = {
 
 gulp.task('css', function(){
   return gulp.src('content/themes/casper/assets/css/screen.css')
-    .pipe(minifyCSS(cssConfig))
+    // .pipe(minifyCSS(cssConfig))
     .pipe(gulp.dest('content/themes/casper/assets/css/dist'))
 })
 
 gulp.task('js', function(){
   return gulp.src(jsSrc)
     .pipe(concat('bundle.min.js'))
-    .pipe(uglify(uglifyConfig))
+    // .pipe(uglify(uglifyConfig))
     .pipe(gulp.dest('content/themes/casper/assets/js/dist'))
 })
 
