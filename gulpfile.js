ce5aeb762153f153ce225ebe8653a11f56856697
spacerockzero <janderson@familysearch.org>
2015-12-18 23:16:50 -0700

    nice working intro image processing build. Resizes, converts to jpg, and optimized images. Cropping is not smart so far, though. Gets 80 percent right automatically

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 /* DEPS */
-var gulp      = require('gulp')
-var minifyCSS = require('gulp-minify-css')
-var uglify    = require('gulp-uglify')
-var concat    = require('gulp-concat')
-var watch     = require('gulp-watch')
-var imageop   = require('gulp-image-optimization')
-var del       = require('del')
+var gulp         = require('gulp')
+var minifyCSS    = require('gulp-minify-css')
+var uglify       = require('gulp-uglify')
+var concat       = require('gulp-concat')
+var watch        = require('gulp-watch')
+var imageop      = require('gulp-image-optimization')
+var del          = require('del')
+var pngquant     = require('imagemin-pngquant')
+var imageminWebp = require('imagemin-webp');
+var resize       = require('gulp-image-resize');
+var rename       = require('gulp-rename');
 
 /* CONFIGS */
 var jsSrc = [
   'content/themes/casper/assets/js/index.js',
   'node_modules/prefetch/prefetch.js',
   'content/themes/casper/assets/js/app.js'
+]
+var imgRawSrc = [
+  'image-proc/raw/**/*.jpg',
+  'image-proc/raw/**/*.jpeg',
+  'image-proc/raw/**/*.JPG',
+  'image-proc/raw/**/*.png',
+  'image-proc/raw/**/*.PNG'
 ]
 
 /* MINIFY CSS CONFIG */
@@ -27,15 +38,52 @@ var uglifyConfig = {}
 var imageOpConfig = {
   optimizationLevel: 5,
   progressive: true,
-  interlaced: true
+  interlaced: true,
+  use: [
+    pngquant( {quality: '65-80', speed: 4} ),
+    imageminWebp( {quality: 50} )
+  ]
 }
 
-gulp.task('del-img', function () {
+gulp.task('img-del', function () {
   del.sync('image-proc/opto/*')
+  del.sync('image-proc/resized/*')
 });
 
-gulp.task('img', ['del-img'], function() {
-  gulp.src('image-proc/raw/**/*')
+gulp.task('img-resize-1024', function () {
+  return gulp.src(imgRawSrc)
+    .pipe(resize({ width: 1024, format: 'jpg' }))
+    .pipe(rename({ suffix: '-1024'}))
+    .pipe(gulp.dest('image-proc/resized'))
+});
+
+gulp.task('img-resize-500', function () {
+  return gulp.src(imgRawSrc)
+    .pipe(resize({ width: 500, format: 'jpg' }))
+    .pipe(rename({ suffix: '-500'}))
+    .pipe(gulp.dest('image-proc/resized'))
+});
+
+gulp.task('img-resize-300', function () {
+  return gulp.src(imgRawSrc)
+    .pipe(resize({ width: 300, format: 'jpg' }))
+    .pipe(rename({ suffix: '-300'}))
+    .pipe(gulp.dest('image-proc/resized'))
+});
+
+gulp.task('img-resize-200sq', function () {
+  return gulp.src(imgRawSrc)
+    .pipe(resize({ crop: true, width: 200, height: 200, format: 'jpg' }))
+    .pipe(rename({ suffix: '-200sq'}))
+    .pipe(gulp.dest('image-proc/resized'))
+});
+
+gulp.task('img-resize', ['img-resize-1024', 'img-resize-500', 'img-resize-300', 'img-resize-200sq'], function(){
+  console.log('img resize complete!')
+})
+
+gulp.task('img', ['img-del', 'img-resize'], function() {
+  return gulp.src('image-proc/resized/**/*')
     .pipe(imageop(imageOpConfig))
     .pipe(gulp.dest('image-proc/opto'))
 })
